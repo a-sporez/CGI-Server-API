30550d6d8c1c33bc885e01785f58228e9d7f1337
a-sporez <adornedspore@gmail.com>
2025-06-20 00:41:15 -0400

    main.js+auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,22 @@
 export async function login(username, password) {
-    
+    const res = await fetch('/users.php', {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({username, password})
+    });
+
+    const data = await res.json();
+    return data.success;
+}
+
+export function onLoginSuccess(username) {
+    document.getElementById('loginForm').style.display = 'none';
+    const panel = document.getElementById('userPanel');
+    panel.style.display = 'block';
+    document.getElementById('userWelcome').textContent = username;
+}
+
+export function logout() {
+    document.getElementById('loginForm').style.display = 'block';
+    document.getElementById('userPanel').style.display = 'none';
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,23 @@
 import { login, logout, onLoginSuccess } from "./auth.js";
 import { fetchUsers } from "./users.js";
+
+const loginForm = document.getElementById('loginForm');
+const logoutBtn = document.getElementById('logoutBtn');
+
+loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const success = await login(username, password);
+    if (success) {
+        onLoginSuccess(username);
+        fetchUsers();
+    } else {
+        alert("Login Failed.");
+    }
+});
+
+logoutBtn.addEventListener('click', () => {
+    logout();
+});

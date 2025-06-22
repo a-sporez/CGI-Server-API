29dd499c1852de5d9d7f54c005bc6be84cbbcc15
a-sporez <adornedspore@gmail.com>
2025-06-21 23:12:54 -0400

    setup db

--- api/setup.php
+++ api/setup.php
@@ -6,8 +6,9 @@ $db = db();
 $db->exec("
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    role TEXT DEFAULT 'user',
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
